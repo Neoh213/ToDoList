484854bebafc4a031dce84a46b9f12b4ecc35273
Neoh213 <djrumph@gmail.com>
2018-02-05 22:39:24 -0500

    changes

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,6 +1,21 @@
 //check off todos by clicking
 
 $("li").click(function(){
-  $(this).css("color", "gray");
-  $(this).css("text-decoration", "line-through")
+  //if li is gray
+  if($(this).css("color") ==="rgb(128, 128, 128)"){
+    //turn it black
+    $(this).css("color","black");
+  }
+
+  //else
+  else{
+    //turn it gray
+    $(this).css({
+      color: "gray",
+      textDecoration: "Line-through"});
+  }
+
+
+
+  //3:10
 });
