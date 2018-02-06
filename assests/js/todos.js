8ef9eb570426186cfa35cd819207f152b5af6bb7
Neoh213 <djrumph@gmail.com>
2018-02-06 18:41:35 -0500

    fully functioning

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,18 +1,26 @@
 //check off todos by clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 $(this).toggleClass("completed");
-
-
 
   //3:10
 });
 
 //click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500,function (){
     $(this).remove();
   });
 
   event.stopPropagation();
 });
+
+$("input[type = 'text']").keypress(function(event){
+  if(event.which ===13){
+    //gets input from the textbox
+  var todoText = ($(this).val());
+  $(this).val("");
+  //create a new li and add to ul
+  $("ul").append("<li><span>X </span> " + todoText + "</li>");
+  }
+})
